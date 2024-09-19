ec617740476aaea5378e9615f2c2ff8856843bd2
Felipe Braiani <felipe.braianitech@gmail.com>
2024-09-19 10:27:06 -0400

    Algumas funcionalidades no Application

--- Application.py
+++ Application.py
@@ -3,3 +3,19 @@ import customtkinter as ctk
 class Application:
     def __init__(self) -> None:
         self.janela = ctk.CTk()
+        self.title = 'PetPals'
+        self.background = 'white'
+        self.set_background(self.background)
+        self.set_geometry(1920,1080)
+
+    def set_geometry(self, width, height):
+        self.janela.attributes('-toolwindow', True)
+
+        self.janela.geometry(f"{width}x{height}+0+0")
+
+    def set_title(self, title=""):
+        self.janela.title(title)
+
+    def set_background(self, color):
+        self.background = color
+        self.janela.config(background=color)
