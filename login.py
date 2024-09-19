2916e502323c54eec58e9d1e5eee46f593b89096
RiquelmeG22 <riquelmeramalho762@gmail.com>
2024-09-19 08:53:19 -0400

    oi
    oi

---
+++ login.py
@@ -0,0 +1,17 @@
+import customtkinter as ctk
+from tkinter import ttk
+from tkinter import messagebox
+from PIL import ImageTk, Image 
+
+
+root = ctk.CTk()
+root.resizable(False, False)
+root.title("Pet Pals")
+root.state("zoomed")
+
+frmtitulo = ctk.CTkLabel(text="Pesquise por Categoria", font=("Arial",15))
+
+
+
+
+
