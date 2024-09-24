6fc88c908b21ea9b1ea31195b6a04cdd0def0b38
RiquelmeG22 <riquelmeramalho762@gmail.com>
2024-09-24 08:40:26 -0400

    ok
    ok

--- telacadastro.py
+++ telacadastro.py
@@ -14,6 +14,8 @@ def submit_login():
     else:
         messagebox.showinfo("Sucesso", f"Login realizado com sucesso, {nome}!")
 
+        
+
 
 root = ctk.CTk()
 root.resizable(False, False)
@@ -28,27 +30,27 @@ frame_header = ctk.CTkFrame(root, fg_color= "white", height=100, corner_radius=0
 frame_header.place(relx=0.5 , rely=0, relwidth=1, anchor="n")
 
 label_texto = ctk.CTkLabel(frame_header, text="PetPal", font=("Arial",20, 'bold'), text_color="black", width=200)
-label_texto.place(x=25, y=35)
+label_texto.place(x=50, y=38)
 
-imgpatacachorro = ("imagens/patacachorro.png")  # "petpals/imagens/patacachorro.png"
-img = Image.open(imgpatacachorro)
-img = img.resize((20,20))
+imglogo = ("imagens/logo.png")  # "petpals/imagens/patacachorro.png"
+img = Image.open(imglogo)
+img = img.resize((70,70))
 img_tk = ImageTk.PhotoImage(img)
 
 label_img = ctk.CTkLabel(frame_header, image=img_tk, text='')
-label_img.place(x=70, y=33)
+label_img.place(x=51, y=15)
 
 inicio = ctk.CTkLabel(frame_header, text="Inicio", font=('Arial',20, 'bold'), text_color="black")
-inicio.place(x=780, y=35)
+inicio.place(x=750, y=35)
 
 loja = ctk.CTkLabel(frame_header, text="Loja", font=('Arial',20, 'bold'), text_color="black")
-loja.place(x=880, y=35)
+loja.place(x=870, y=35)
 
 sobre = ctk.CTkLabel(frame_header, text="Sobre", font=('Arial',20, 'bold'), text_color="black")
-sobre.place(x=980, y=35)
+sobre.place(x=970, y=35)
 
 contato = ctk.CTkLabel(frame_header, text="Contato", font=('Arial',20, 'bold'), text_color="black")
-contato.place(x=1080, y=35)
+contato.place(x=1060, y=35)
 
 # Imagem de Fundo da Tela de Cadastro!!
 
@@ -66,12 +68,12 @@ label_fundo = ctk.CTkLabel(root, image=tk_fundo, text='', width=width_tela)
 label_fundo.place(x=0, y=100)
 
 frame_login = ctk.CTkFrame(root, width=380, height=450, fg_color='#f8f9fa', corner_radius=25, bg_color="#fafafa")
-frame_login.place(x=820, y=270)
+frame_login.place(x=765, y=260)
 pywinstyles.set_opacity(frame_login, color="#fafafa")
 
 nome_completo = ctk.CTkLabel(frame_login, text="Nome Completo", font=('Arial', 16, 'bold'), text_color='black')
 nome_completo.place(x=35, y=50)
-entry_nomecompleto = ctk.CTkEntry(frame_login, width=300, fg_color='white', border_width=0)
+entry_nomecompleto = ctk.CTkEntry(frame_login, width=300, fg_color='white', border_width=0, height=30)
 entry_nomecompleto.place(x=35, y=80)
 
 email = ctk.CTkLabel(frame_login, text="Email", font=('Arial', 16, 'bold'), text_color='black')
@@ -92,11 +94,11 @@ entry_confirmar_senha.place(x=35, y=260)
 
 check_var = ctk.BooleanVar()
 checkbox_termos = ctk.CTkCheckBox(frame_login, text="Li e concordo com os termos de uso", variable=check_var,
- font=("Arial",12, 'bold'), text_color='black')
+ font=("Arial",12, 'bold'), text_color='black', corner_radius=100)
 checkbox_termos.place(x=45, y=300)
 
 check_var_termo2 = ctk.BooleanVar()
-checkbox_termo2 = ctk.CTkCheckBox(frame_login, text="Li e concordo com as politicas da empresa", variable=check_var_termo2, font=("Arial",12, 'bold'), text_color='black')
+checkbox_termo2 = ctk.CTkCheckBox(frame_login, text="Li e concordo com as politicas da empresa", variable=check_var_termo2, font=("Arial",12, 'bold'), text_color='black', corner_radius=100)
 checkbox_termo2.place(x=45, y=330)
 
 botao = ctk.CTkButton(frame_login, text="Cadastrar", font=("Arial",17), command=submit_login, 
