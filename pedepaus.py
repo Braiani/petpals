6adbf01c606a8fb4bbac3f15653c503311af32bd
H3nriqueSB <henrique49880516@aluno.ms.senac.br>
2024-09-24 10:54:13 -0400

    finalizado
    terminado?

--- pedepaus.py
+++ pedepaus.py
@@ -5,6 +5,7 @@ app = ctk.CTk()
 app.geometry('1920x1080')
 app.configure(fg_color='Seashell')
 
+
 framefundo = ctk.CTkFrame(app, width=1880, height=85, fg_color='white', corner_radius=60)
 framefundo.place(x=20, y=122)
 
@@ -23,7 +24,7 @@ framePetPaw.place(x=860, y=150,)
 framecateg = ctk.CTkLabel(app, text='CATEGORIAS', text_color='black', font=('', 40, 'bold'), width=200, fg_color='Seashell')
 framecateg.place(x=825, y=250,) 
 
-img_path = 'imagens\phone.png'
+img_path = 'imagens/phone.png'
 img = Image.open(img_path) 
 img = img.resize((30, 30))
 img_tk = ImageTk.PhotoImage(img)
@@ -31,7 +32,7 @@ img_tk = ImageTk.PhotoImage(img)
 label_img = ctk.CTkLabel(app, image=img_tk, text='')
 label_img.place(x=350, y=38)
 
-img_path = 'imagens\email.png'
+img_path = 'imagens/email.png'
 img = Image.open(img_path) 
 img = img.resize((30, 30))
 img_tk = ImageTk.PhotoImage(img)
@@ -39,7 +40,7 @@ img_tk = ImageTk.PhotoImage(img)
 label_img = ctk.CTkLabel(app, image=img_tk, text='')
 label_img.place(x=550, y=38)
 
-img_path = 'imagens\location-16-regular.png'
+img_path = 'imagens/location-16-regular.png'
 img = Image.open(img_path) 
 img = img.resize((30, 30))
 img_tk = ImageTk.PhotoImage(img)
@@ -47,7 +48,7 @@ img_tk = ImageTk.PhotoImage(img)
 label_img = ctk.CTkLabel(app, image=img_tk, text='')
 label_img.place(x=1230, y=38)
 
-img_path = 'imagens\casadog.png'
+img_path = 'imagens/casadog.png'
 img = Image.open(img_path) 
 img = img.resize((70, 70))
 img_tk = ImageTk.PhotoImage(img)
@@ -58,77 +59,116 @@ label_img.place(x=860, y=130)
 frameinicio = ctk.CTkLabel(app, text='', width=140, fg_color='darkorange')
 frameinicio.place(x=50, y=153) 
 
-def on_enter(button):
-    button.configure(fg_color='darkorange')
-def on_leave(button):
-    button.configure(fg_color='orange')
 
-btinicio = ctk.CTkButton(app, text='INICIO', corner_radius=0, fg_color='orange')
+btinicio = ctk.CTkButton(app, text='INICIO', corner_radius=0, fg_color='orange', hover_color='darkorange')
 btinicio.place(x=50, y=150)
-btinicio.bind("<Enter>", lambda event: on_enter(btinicio))
-btinicio.bind("<Leave>", lambda event: on_leave(btinicio))
+
 
 frameprod = ctk.CTkLabel(app, text='', width=140, fg_color='darkorange')
 frameprod.place(x=200, y=153) 
 
-btprod = ctk.CTkButton(app, text='PRODUTOS', corner_radius=0, fg_color='orange')
+btprod = ctk.CTkButton(app, text='PRODUTOS', corner_radius=0, fg_color='orange', hover_color='darkorange')
 btprod.place(x=200, y=150)
-btprod.bind("<Enter>", lambda event: on_enter(btprod))
-btprod.bind("<Leave>", lambda event: on_leave(btprod))
+
 
 framesobre = ctk.CTkLabel(app, text='', width=140, fg_color='darkorange')
 framesobre.place(x=350, y=153) 
 
-btsobre = ctk.CTkButton(app, text='SOBRE', corner_radius=0, fg_color='orange')
+btsobre = ctk.CTkButton(app, text='SOBRE', corner_radius=0, fg_color='orange', hover_color='darkorange')
 btsobre.place(x=350, y=150)
-btsobre.bind("<Enter>", lambda event: on_enter(btsobre))
-btsobre.bind("<Leave>", lambda event: on_leave(btsobre))
+
 
 framecomu = ctk.CTkLabel(app, text='', width=140, fg_color='darkorange')
 framecomu.place(x=500, y=153) 
 
-btcomu = ctk.CTkButton(app, text='COMUNIDADE', corner_radius=0, fg_color='orange')
+btcomu = ctk.CTkButton(app, text='COMUNIDADE', corner_radius=0, fg_color='orange', hover_color='darkorange')
 btcomu.place(x=500, y=150)
-btcomu.bind("<Enter>", lambda event: on_enter(btcomu))
-btcomu.bind("<Leave>", lambda event: on_leave(btcomu))
+
 
 buscar = ctk.CTkEntry(app, placeholder_text='Buscar...', width=250, height=31, corner_radius=50)
 buscar.place(x=1520, y=150)
 
-framebusc = ctk.CTkLabel(app, text='', width=45, height=30, corner_radius=10, fg_color='darkorange')
-framebusc.place(x=1750, y=151) 
+framebusc = ctk.CTkLabel(app, text='', width=28, height=30, corner_radius=0, fg_color='darkorange')
+framebusc.place(x=1750, y=150) 
 
-imagem_busca = Image.open('imagens\lupa.png')
+imagem_busca = Image.open('imagens/lupa.png')
 imagem_busca = imagem_busca.resize((20, 20))
 imagem_tk = ImageTk.PhotoImage(imagem_busca)
 
-busca = ctk.CTkButton(app, image=imagem_tk, text='', width=20, height=20, corner_radius=25, fg_color='orange')
+busca = ctk.CTkButton(app, image=imagem_tk, text='', width=20, height=20, corner_radius=0, fg_color='orange', hover_color='darkorange')
 busca.place(x=1750, y=150)
 busca.image = imagem_tk
-busca.bind("<Enter>", lambda event: on_enter(busca))
-busca.bind("<Leave>", lambda event: on_leave(busca))
 
-imagem_wishlist = Image.open('imagens\heart-twotone.png')
+
+imagem_wishlist = Image.open('imagens/heart-twotone.png')
 imagem_wishlist = imagem_wishlist.resize((20, 20))
 imagem_tk = ImageTk.PhotoImage(imagem_wishlist)
 
-wishlist = ctk.CTkButton(app, image=imagem_tk, text='', width=20, height=20, corner_radius=25, fg_color='white')
+wishlist = ctk.CTkButton(app, image=imagem_tk, text='', width=20, height=20, corner_radius=25, fg_color='white', hover_color='darkorange')
 wishlist.place(x=1800, y=150)
 wishlist.image = imagem_tk
 
-imagem_cart = Image.open('imagens\cart-duotone.png')
+imagem_cart = Image.open('imagens/cart-duotone.png')
 imagem_cart = imagem_cart.resize((20, 20))
 imagem_tk = ImageTk.PhotoImage(imagem_cart)
 
-cart = ctk.CTkButton(app, image=imagem_tk, text='', width=20, height=20, corner_radius=25, fg_color='white')
+cart = ctk.CTkButton(app, image=imagem_tk, text='', width=20, height=20, corner_radius=25, fg_color='white', hover_color='darkorange')
 cart.place(x=1850, y=150)
 cart.image = imagem_tk
 
-img_path = 'imagens\Footer 1.png'
+frameprod2 = ctk.CTkLabel(app, font=('', 25, 'bold'), text='Acessórios', text_color='black', height=300, width=300, corner_radius=25)
+frameprod2.place(x=280, y=350)
+
+img_path = 'imagens/coleira.png'
+img = Image.open(img_path) 
+img = img.resize((300, 300))
+img_tk = ImageTk.PhotoImage(img)
+
+label_img = ctk.CTkLabel(app, image=img_tk, text='', height=300, width=300, corner_radius=25)
+label_img.place(x=280, y=350)
+
+frameprod2 = ctk.CTkLabel(app, font=('', 25, 'bold'), text='Acessórios', text_color='black', height=100, width=320, corner_radius=50)
+frameprod2.place(x=280, y=580)
+
+img_path = 'imagens/alimento.png'
+img = Image.open(img_path) 
+img = img.resize((300, 300))
+img_tk = ImageTk.PhotoImage(img)
+
+label_img = ctk.CTkLabel(app, image=img_tk, text='', height=300, width=300, corner_radius=25)
+label_img.place(x=600, y=350)
+
+frameprod2 = ctk.CTkLabel(app, font=('', 25, 'bold'), text='Alimentação', text_color='black', height=100, width=320, corner_radius=50)
+frameprod2.place(x=620, y=580)
+
+img_path = 'imagens/brinquedo.png'
+img = Image.open(img_path) 
+img = img.resize((300, 300))
+img_tk = ImageTk.PhotoImage(img)
+
+label_img = ctk.CTkLabel(app, image=img_tk, text='', height=300, width=300, corner_radius=25)
+label_img.place(x=940, y=350)
+
+frameprod2 = ctk.CTkLabel(app, font=('', 25, 'bold'), text='Brinquedos', text_color='black', height=100, width=320, corner_radius=50)
+frameprod2.place(x=960, y=580)
+
+img_path = 'imagens/shampoo.png'
+img = Image.open(img_path) 
+img = img.resize((300, 300))
+img_tk = ImageTk.PhotoImage(img)
+
+label_img = ctk.CTkLabel(app, image=img_tk, text='', height=300, width=300, corner_radius=25)
+label_img.place(x=1260, y=350)
+# label_img.bind('<Button-1>', nova_tela())
+
+frameprod2 = ctk.CTkLabel(app, font=('', 25, 'bold'), text='Higiene', text_color='black', height=100, width=320, corner_radius=50)
+frameprod2.place(x=1280, y=580)
+
+img_path = 'imagens/Footer 1.png'
 img = Image.open(img_path) 
 img_tk = ImageTk.PhotoImage(img)
 
 label_img = ctk.CTkLabel(app, image=img_tk, text='')
-label_img.place(x=0, y=820)
+label_img.place(x=0, y=852)
 
 app.mainloop()
