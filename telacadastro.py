ac9fee5d0a612df2fdc375fdaabb3eff696bd99c
RiquelmeG22 <riquelmeramalho762@gmail.com>
2024-09-24 08:05:25 -0400

    ok
    ok

--- telacadastro.py
+++ telacadastro.py
@@ -39,16 +39,16 @@ label_img = ctk.CTkLabel(frame_header, image=img_tk, text='')
 label_img.place(x=70, y=33)
 
 inicio = ctk.CTkLabel(frame_header, text="Inicio", font=('Arial',20, 'bold'), text_color="black")
-inicio.place(x=800, y=35)
+inicio.place(x=780, y=35)
 
 loja = ctk.CTkLabel(frame_header, text="Loja", font=('Arial',20, 'bold'), text_color="black")
-loja.place(x=900, y=35)
+loja.place(x=880, y=35)
 
 sobre = ctk.CTkLabel(frame_header, text="Sobre", font=('Arial',20, 'bold'), text_color="black")
-sobre.place(x=1000, y=35)
+sobre.place(x=980, y=35)
 
 contato = ctk.CTkLabel(frame_header, text="Contato", font=('Arial',20, 'bold'), text_color="black")
-contato.place(x=1100, y=35)
+contato.place(x=1080, y=35)
 
 # Imagem de Fundo da Tela de Cadastro!!
 
@@ -65,28 +65,28 @@ width_tela = root.winfo_width()
 label_fundo = ctk.CTkLabel(root, image=tk_fundo, text='', width=width_tela)
 label_fundo.place(x=0, y=100)
 
-frame_login = ctk.CTkFrame(root, width=380, height=450, fg_color='#F8F9FA', corner_radius=25, bg_color="#ffffff")
+frame_login = ctk.CTkFrame(root, width=380, height=450, fg_color='#f8f9fa', corner_radius=25, bg_color="#fafafa")
 frame_login.place(x=820, y=270)
-pywinstyles.set_opacity(frame_login, color="#ffffff")
+pywinstyles.set_opacity(frame_login, color="#fafafa")
 
 nome_completo = ctk.CTkLabel(frame_login, text="Nome Completo", font=('Arial', 16, 'bold'), text_color='black')
 nome_completo.place(x=35, y=50)
-entry_nomecompleto = ctk.CTkEntry(frame_login, width=300)
+entry_nomecompleto = ctk.CTkEntry(frame_login, width=300, fg_color='white', border_width=0)
 entry_nomecompleto.place(x=35, y=80)
 
 email = ctk.CTkLabel(frame_login, text="Email", font=('Arial', 16, 'bold'), text_color='black')
 email.place(x=35, y=110)
-entry_email = ctk.CTkEntry(frame_login,width=300)
+entry_email = ctk.CTkEntry(frame_login,width=300, fg_color='white', border_width=0)
 entry_email.place(x=35, y=140)
 
 senha = ctk.CTkLabel(frame_login, text="Senha", font=('Arial', 16, 'bold'), text_color='black')
 senha.place(x=35, y=170)
-entry_senha = ctk.CTkEntry(frame_login, show='*', width=300) 
+entry_senha = ctk.CTkEntry(frame_login, show='*', width=300, fg_color='white', border_width=0) 
 entry_senha.place(x=35, y=200)
 
 confirmar_senha = ctk.CTkLabel(frame_login, text="Confirmar Senha", font=('Arial', 16, 'bold'), text_color='black')
 confirmar_senha.place(x=35, y=230)
-entry_confirmar_senha = ctk.CTkEntry(frame_login, show='*', width=300)
+entry_confirmar_senha = ctk.CTkEntry(frame_login, show='*', width=300, fg_color='white', border_width=0)
 entry_confirmar_senha.place(x=35, y=260)
 
 
@@ -101,8 +101,20 @@ checkbox_termo2.place(x=45, y=330)
 
 botao = ctk.CTkButton(frame_login, text="Cadastrar", font=("Arial",17), command=submit_login, 
                                fg_color="orange", text_color="white",  width=150, height=50)
-botao.place(x=120, y=380)
+botao.place(x=110, y=380)
 
 #Rodapé da Tela de Cadastro!!
+
+
+img_rodape = ("imagens/rodape.png")  # "petpals/imagens/patacachorro.png"
+rodape = Image.open(img_rodape)
+# rodape = img.resize((20,20))
+rodape_img = ImageTk.PhotoImage(rodape)
+
+label_rodape = ctk.CTkLabel(root, image=rodape_img, text='')
+label_rodape.place(x=0, y=860)
+
+
+
 
 root.mainloop()
