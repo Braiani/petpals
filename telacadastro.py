9abd938979c8ea1e7163fdf06d38b5e2bda4308c
RiquelmeG22 <riquelmeramalho762@gmail.com>
2024-09-24 10:34:12 -0400

    terminando
    ok

--- telacadastro.py
+++ telacadastro.py
@@ -11,10 +11,8 @@ def submit_login():
     
     if senha != confirmar_senha:
         messagebox.showerror("Erro", "As senhas não coincidem.")
-    else:
-        messagebox.showinfo("Sucesso", f"Login realizado com sucesso, {nome}!")
-
-        
+        return
+    messagebox.showinfo("Sucesso", f"Login realizado com sucesso, {nome}!")
 
 
 root = ctk.CTk()
@@ -41,16 +39,16 @@ label_img = ctk.CTkLabel(frame_header, image=img_tk, text='')
 label_img.place(x=51, y=15)
 
 inicio = ctk.CTkLabel(frame_header, text="Inicio", font=('Arial',20, 'bold'), text_color="black")
-inicio.place(x=750, y=35)
+inicio.place(x=800, y=35)
 
 loja = ctk.CTkLabel(frame_header, text="Loja", font=('Arial',20, 'bold'), text_color="black")
-loja.place(x=870, y=35)
+loja.place(x=900, y=35)
 
 sobre = ctk.CTkLabel(frame_header, text="Sobre", font=('Arial',20, 'bold'), text_color="black")
-sobre.place(x=970, y=35)
+sobre.place(x=1000, y=35)
 
 contato = ctk.CTkLabel(frame_header, text="Contato", font=('Arial',20, 'bold'), text_color="black")
-contato.place(x=1060, y=35)
+contato.place(x=1100, y=35)
 
 # Imagem de Fundo da Tela de Cadastro!!
 
@@ -67,43 +65,44 @@ width_tela = root.winfo_width()
 label_fundo = ctk.CTkLabel(root, image=tk_fundo, text='', width=width_tela)
 label_fundo.place(x=0, y=100)
 
-frame_login = ctk.CTkFrame(root, width=380, height=450, fg_color='#f8f9fa', corner_radius=25, bg_color="#fafafa")
-frame_login.place(x=765, y=260)
+frame_login = ctk.CTkFrame(root, width=309, height=521, fg_color='#F8F9FA', corner_radius=25, bg_color="#fafafa")
+frame_login.place(x=845, y=220)
 pywinstyles.set_opacity(frame_login, color="#fafafa")
 
 nome_completo = ctk.CTkLabel(frame_login, text="Nome Completo", font=('Arial', 16, 'bold'), text_color='black')
-nome_completo.place(x=35, y=50)
-entry_nomecompleto = ctk.CTkEntry(frame_login, width=300, fg_color='white', border_width=0, height=30)
-entry_nomecompleto.place(x=35, y=80)
+nome_completo.place(x=20, y=50)
+entry_nomecompleto = ctk.CTkEntry(frame_login, width=250, fg_color='white', border_width=0, height=30)
+entry_nomecompleto.place(x=20, y=80)
 
 email = ctk.CTkLabel(frame_login, text="Email", font=('Arial', 16, 'bold'), text_color='black')
-email.place(x=35, y=110)
-entry_email = ctk.CTkEntry(frame_login,width=300, fg_color='white', border_width=0)
-entry_email.place(x=35, y=140)
+email.place(x=20, y=125)
+entry_email = ctk.CTkEntry(frame_login,width=250, fg_color='white', border_width=0, height=30)
+entry_email.place(x=20, y=155)
 
 senha = ctk.CTkLabel(frame_login, text="Senha", font=('Arial', 16, 'bold'), text_color='black')
-senha.place(x=35, y=170)
-entry_senha = ctk.CTkEntry(frame_login, show='*', width=300, fg_color='white', border_width=0) 
-entry_senha.place(x=35, y=200)
+senha.place(x=20, y=185)
+entry_senha = ctk.CTkEntry(frame_login, show='*', width=250, fg_color='white', border_width=0, height=30) 
+entry_senha.place(x=20, y=220)
 
 confirmar_senha = ctk.CTkLabel(frame_login, text="Confirmar Senha", font=('Arial', 16, 'bold'), text_color='black')
-confirmar_senha.place(x=35, y=230)
-entry_confirmar_senha = ctk.CTkEntry(frame_login, show='*', width=300, fg_color='white', border_width=0)
-entry_confirmar_senha.place(x=35, y=260)
+confirmar_senha.place(x=20, y=255)
+entry_confirmar_senha = ctk.CTkEntry(frame_login, show='*', width=250, fg_color='white', border_width=0, height=30)
+entry_confirmar_senha.place(x=20, y=290)
 
 
 check_var = ctk.BooleanVar()
 checkbox_termos = ctk.CTkCheckBox(frame_login, text="Li e concordo com os termos de uso", variable=check_var,
  font=("Arial",12, 'bold'), text_color='black', corner_radius=100)
-checkbox_termos.place(x=45, y=300)
+checkbox_termos.place(x=20, y=340)
 
 check_var_termo2 = ctk.BooleanVar()
-checkbox_termo2 = ctk.CTkCheckBox(frame_login, text="Li e concordo com as politicas da empresa", variable=check_var_termo2, font=("Arial",12, 'bold'), text_color='black', corner_radius=100)
-checkbox_termo2.place(x=45, y=330)
+checkbox_termo2 = ctk.CTkCheckBox(frame_login, text="Li e concordo com as politicas da empresa",
+ variable=check_var_termo2, font=("Arial",12, 'bold'), text_color='black', corner_radius=100)
+checkbox_termo2.place(x=20, y=390)
 
 botao = ctk.CTkButton(frame_login, text="Cadastrar", font=("Arial",17), command=submit_login, 
-                               fg_color="orange", text_color="white",  width=150, height=50)
-botao.place(x=110, y=380)
+                               fg_color="#FD7E14", text_color="white",  width=135, height=45, corner_radius=8)
+botao.place(x=80, y=450)
 
 #Rodapé da Tela de Cadastro!!
 
