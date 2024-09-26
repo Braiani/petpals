d83bc0e8e5ce366109ea1ecb0134024e43e72bb0
Felipe Braiani <felipe.braianitech@gmail.com>
2024-09-26 09:20:23 -0400

    Ajustes para novo padrão de header

--- Main.py
+++ Main.py
@@ -12,15 +12,5 @@ class Main(Application):
 root = Main()
 root.top_header()
 
-frame = root.set_frame(position={
-    'y': 88,
-    'x': 312
-}, options={
-    'width': 1216,
-    'bg_color': 'black',
-    'fg_color': 'white',
-    'border_color': 'white',
-    'border_width': 40
-})
 
 root.start()
